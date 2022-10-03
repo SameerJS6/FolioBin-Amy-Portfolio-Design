835207417e3011299d727a65e075642ad8951f06
Sameer Singh <sameersingh1234pok@gmail.com>
2022-10-03 22:15:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: false,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  centerMode: true,
+  arrows: true,
+  skipBtn: false,
+});
